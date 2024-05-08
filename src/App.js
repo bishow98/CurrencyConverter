3bedc59aff44101b42977c6917c8172f3c7af963
Bishow Chauhan <chauhandavid4199@gmail.com>
2024-05-08 11:17:20 +0545

    simple realtime currency converter using useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [currency, setCurrency] = useState(10);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConvertedValue] = useState();
+  const [loading, setIsloading] = useState(false);
+
+  useEffect(
+    function () {
+      async function request() {
+        setIsloading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${currency}&from=${fromCur}&to=${toCur}`
+        );
+        const data = await res.json();
+        console.log(data.rates[toCur]);
+        setConvertedValue(Number(data.rates[toCur]));
+        setIsloading(false);
+      }
+      if (fromCur === toCur) {
+        setConvertedValue(currency);
+      } else {
+        request();
+      }
+    },
+    [currency, fromCur, toCur]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <input
+        type="text"
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value)}
+        disabled={loading}
+      />
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={loading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={loading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      {currency ? (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {converted}
+          {toCur}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
-export default App;
